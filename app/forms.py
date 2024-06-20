df3af6181b8961acb1bc142fa10440bb261742ab
GuilhermeCAz <102599422+GuilhermeCAz@users.noreply.github.com>
2024-06-20 00:41:56 -0300

    Form attributes defined

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,13 @@ from django import forms
 class URLForm(forms.Form):
     original_url = forms.URLField(
         label='Enter URL',
-        widget=forms.URLInput(attrs={'placeholder': 'https://example.com'}),
+        widget=forms.URLInput(
+            attrs={
+                'placeholder': 'https://example.com',
+                'class': 'form-control',
+                'aria-label': 'Enter with URL...',
+                'aria-describedby': 'button-addon2',
+            }
+        ),
         required=True,
     )
